5bf2d45a675d6fcab37d43fbb0eed320757e66a7
karthik potnuru <potnuru.karthik@continental-corporation.com>
2024-11-06 16:16:13 +0000

    fix terraform resource name errors

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -129,14 +129,14 @@ resource "aws_iam_policy" "jenkins_policy" {
 
 # Attach Jenkins Deployment Policy to Role
 resource "aws_iam_role_policy_attachment" "jenkins_deployment_policy_attachment" {
-  role       = aws_iam_role.jenkins_instance_role.name
-  policy_arn = aws_iam_policy.jenkins_deployment_policy.arn
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.jenkins_policy.arn
 }
 
 # Create Instance Profile for Jenkins EC2 Instance
 resource "aws_iam_instance_profile" "jenkins_instance_profile" {
   name = "jenkins-instance-profile"
-  role = aws_iam_role.jenkins_instance_role.name
+  role = aws_iam_role.jenkins_role.name
 }
 
 # EC2 Instance for Jenkins
